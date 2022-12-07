8702007c8ad4f3dfbec3d911fc8524d325e69d02
Gustavo Rocha <111547125+gust-tech@users.noreply.github.com>
2022-12-07 17:42:31 -0300

    Atualização no Navbar

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import { Box } from '@mui/material';
 import './Navbar.css'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
 
 function Navbar() {
+
+    const [token, setToken] = useLocalStorage('token')
+    let navigate = useNavigate()
+
+    function goLogout(){
+        setToken('')
+        alert("Conta desconectada com sucesso!")
+        navigate('/login')
+    }
+
     return (
         <>
             <AppBar position="static">
@@ -25,22 +36,35 @@ function Navbar() {
                         </Box>
                         <Box mx={1} className='Cursor'>
                             <Typography variant="h6" color="inherit">
+                                <Link to='/posts' className='textobranco'>
                                 Postagens
+                                </Link>
                             </Typography>
                         </Box>
                         <Box mx={1} className='Cursor'>
                             <Typography variant="h6" color="inherit">
+                                <Link to='/formularioPostagem' className='textobranco'>
+                                Cadastrar Postagem
+                                </Link>
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='Cursor'>
+                            <Typography variant="h6" color="inherit">
+                                <Link to='/temas' className='textobranco'>
                                 Temas
+                                </Link>
                             </Typography>
                         </Box>
                         <Box mx={1} className='Cursor'>
                             <Typography variant="h6" color="inherit">
+                                <Link to='/formularioTema' className='textobranco'>
                                 Cadastrar Tema
+                                </Link>
                             </Typography>
                         </Box>
-                        <Box mx={1} className='Cursor'>
+                        <Box mx={1} className='Cursor' onClick={goLogout}>
                             <Typography variant="h6" color="inherit">
-                                Logout
+                                Sair
                             </Typography>
                         </Box>
                     </Box>
