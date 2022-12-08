c00184dcbc3fecf92cc3104eb711ecb492572eda
Gustavo Rocha <111547125+gust-tech@users.noreply.github.com>
2022-12-08 09:45:20 -0300

    Atualização Redux

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 import './App.css';
 import ListaTema from './components/temas/listatema/ListaTema';
@@ -11,46 +12,48 @@ import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
 import CadastroTema from './components/temas/cadastroTema/CadastroTema';
 import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
 import DeletarTema from './components/temas/deletarTema/DeletarTema';
-import Home from './components/estaticos/home/Home';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-    
-        <div style={{ minHeight: '100vh' }}>
-        <Routes>
-        <Route path="/" element={<Login />} />
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+       
+          <div style={{ minHeight: '100vh' }}>
+          <Routes>
 
-        <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
 
-        <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
 
-        <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+            <Route path="/home" element={<Home />} />
 
-        <Route path="/temas" element={<ListaTema />} />
+            <Route path="/cadastrousuario" element={<CadastroUsuario />} />
 
-        <Route path="/posts" element={<ListaPostagem />} />
+            <Route path="/temas" element={<ListaTema />} />
 
-        <Route path="/formularioPostagem" element={<CadastroPost />} />
+            <Route path="/posts" element={<ListaPostagem />} />
 
-        <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+            <Route path="/formularioPostagem" element={<CadastroPost />} />
 
-        <Route path="/formularioTema" element={<CadastroTema />} />
+            <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
 
-        <Route path="/formularioTema/:id" element={<CadastroTema />} />
+            <Route path="/formularioTema" element={<CadastroTema />} />
 
-        <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+            <Route path="/formularioTema/:id" element={<CadastroTema />} />
 
-        <Route path="/deletarTema/:id" element={<DeletarTema />} />
+            <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
 
-
-        </Routes>
-        </div>
-     
-      <Footer />
-    </Router>
+            <Route path="/deletarTema/:id" element={<DeletarTema />} />
+            </Routes>
+          </div>
+      
+        <Footer />
+      </Router>
+    </Provider>
   );
 }
 
