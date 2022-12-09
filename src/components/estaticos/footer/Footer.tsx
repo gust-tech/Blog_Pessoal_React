3d76656da2431564cf0c0820405ad1728aebc708
Gustavo Rocha <111547125+gust-tech@users.noreply.github.com>
2022-12-09 16:40:48 -0300

    Update Footer.tsx

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -6,9 +6,12 @@ import { Box } from '@mui/material';
 import './Footer.css'
 
 function Footer() {
-    return (
-        <>
-            <Grid container direction="row" justifyContent="center" alignItems="center">
+        const token = useSelector<TokenState, TokenState["tokens"]>(
+            (state) => state.tokens
+          );
+          var footerComponent;
+          if(token != ""){
+            footerComponent = Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
                     <Box className='box1'>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
@@ -37,8 +40,13 @@ function Footer() {
                     </Box>
                 </Grid>
             </Grid>
+          }
+
+    return (
+        <>
+        {footerComponent}
         </>
-    )
+    );
 }
 
 export default Footer;
